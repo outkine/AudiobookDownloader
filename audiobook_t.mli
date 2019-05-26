500f538996bb0283e2c60f269515b8c44d1e91f8
Anton <antonoutkine@gmail.com>
2019-05-25 20:50:36 -0500

    Add atd alias

--- audiobook_t.mli
+++
@@ -1,8 +0,0 @@
-(* Auto-generated from "audiobook.atd" *)
-              [@@@ocaml.warning "-27-32-35-39"]
-
-type root = { aItems: string }
-
-type book = { title: string; mp3: string }
-
-type books = book list
